2a3084525813ba82c49c00eaaf113467458929e8
Prof. Thiago Vieira de Aguiar <56936784+thiago-aguiar@users.noreply.github.com>
2023-03-01 22:37:40 +0000

    Aula 06 - 27/02/2023: For, For Of e For In.

--- script.js
+++ script.js
@@ -1,102 +1,82 @@
 const mainApp = document.getElementById("app");
 
-mainApp.innerHTML += `<h1>Aula 05</h1>`;
+mainApp.innerHTML += `<h1>Aula 06</h1>`;
 mainApp.innerHTML += `<h2>Estutura de Repetição</h2>`;
 
-mainApp.innerHTML += `<h3>Break / Continue</h3>`;
+const clientes = [
+  { nome: "Fulano" },
+  { nome: "Ciclano" },
+  { nome: "Beltrano" }
+];
 
-mainApp.innerHTML += `<h2>Break</h2>`;
+let tabela = "<table>";
+tabela += "<thead><tr><th>Nome</th></tr><thread>"
+tabela += "<tbody>";
+tabela += "<tr><td>Fulano</td></tr>";
+tabela += "<tr><td>Ciclano</td></tr>";
+tabela += "<tr><td>Beltrano</td></tr>";
+tabela += "</tbody>"
+tabela += "</table>";
+mainApp.innerHTML += tabela;
 
-let contagem = 0;
+mainApp.innerHTML += `<h3>for Each</h3>`;
 
-while (contagem <= 10) {
-  contagem++;
-  if (contagem == 8) break;
-  mainApp.innerHTML += `<p>${contagem}</p>`;
+
+clientes.forEach(function(cliente) {
+  mainApp.innerHTML += `<p>${cliente.nome}</p>`;
+});
+
+
+mainApp.innerHTML += `<h3>for in</h3>`;
+
+const pessoa = {
+  nome: "Fulano", idade: 90, end: "RJ", id: 1
+};
+mainApp.innerHTML += `<p>Nome: ${pessoa.nome}</p>`;
+mainApp.innerHTML += `<p>Nome: ${pessoa['nome']}</p>`;
+for (let prop in pessoa) {
+  mainApp.innerHTML += `<p>${prop}: ${pessoa[prop]}</p>`;
 }
 
-mainApp.innerHTML += `<h2>Continue</h2>`;
-
-contagem = 0;
-while (contagem <= 10) {
-  contagem++;
-  if (contagem == 8) {
-    mainApp.innerHTML += `<p>Encontrei o 8</p>`;
-    continue;
-  }
-  mainApp.innerHTML += `<p>${contagem}</p>`;
+mainApp.innerHTML += `<h3>for of</h3>`;
+// const nomes = ["Fulano", "Ciclano", "Beltrano"];
+/* for (let nome of nomes) {
+  mainApp.innerHTML += `<p>${nome}</p>`;
+} */
+const pessoas = [
+  { nome: "Fulano" },
+  { nome: "Ciclano" },
+  { nome: "Beltrano" }
+];
+for (let pessoa of pessoas) {
+  mainApp.innerHTML += `<p>${pessoa.nome}</p>`;
 }
 
-mainApp.innerHTML += `<h3>Do .. While</h3>`;
-
-mainApp.innerHTML += `<h2>Menu</h2>`;
-
-let opcao = 0;
-let opcoesValidas = [0, 1, 2];
-
-/* while (true) {
-  // menu
-  // break;
+/* for (let i = 0; i < nomes.length; i++) {
+  mainApp.innerHTML += `<p>${nomes[i]}</p>`;
 } */
 
-while (false || !opcoesValidas.includes(opcao)) {
-  console.log(`1 - Inserir`);
-  console.log(`2 - Excluir`);
-  console.log(`0 - Sair`);
-  // opcao = +prompt(`Escolha uma opção:`);
-}
 
-do {
-  console.log(`1 - Inserir`);
-  console.log(`2 - Excluir`);
-  console.log(`0 - Sair`);
-  // opcao = +prompt(`Escolha uma opção:`);
-} while (false || !opcoesValidas.includes(opcao));
+mainApp.innerHTML += `<h3>FOR</h3>`;
 
-
-let numDo = 0;
-do {
-  mainApp.innerHTML += `<p>${numDo}</p>`;
-  numDo++;
-} while (numDo < 10);
-
-
-
-mainApp.innerHTML += `<h3>While</h3>`;
-
-// Dado um array de nomes, faça um script que verifique se um determinado nome está neste array, usando while.
-
-let nomes = ["Fulano", "Ciclano", "Beltrano", "Xtrano", "Deltrano"];
-
-let naoEncontrado = true;
-let mensagem = "Não encontrado.";
-// let indice = 0;
-
-// while (naoEncontrado && indice < nomes.length > 0) {
-/* while (naoEncontrado && nomes.length > 0) {
-  if (nomes[0] === "Htrano"){
-    mensagem = "Encontrado.";
-    naoEncontrado = false;
-  } else {
-    nomes.shift();
-  }
-} */
-mainApp.innerHTML += `<p>${mensagem}</p>`;
-
-
-
-let num = 0;
-while (num < 10) { // num == 10
-  mainApp.innerHTML += `<p>${num}</p>`;
-  num++;
-}
-mainApp.innerHTML += `<p>Fim do While.</p>`;
-
-/* let numB = 0
-let numA = 100
-for (let numB = 0; numB < 100 || numA > 1000;) {
-  numA++;
-  numB++;
-  mainApp.innerHTML += `<p>${num}</p>`;
+/* for (let i = 0; i < 10; i++) {
+  mainApp.innerHTML += `<p>${i}</p>`;
 } */
 
+/* let i = 13;
+for (; i < 15; i++) {
+  mainApp.innerHTML += `<p>${i}</p>`;
+} */
+
+/* let i = 20;
+for (;; i++) {
+  if (i >= 30) break;
+  mainApp.innerHTML += `<p>${i}</p>`;
+} */
+
+/* let i = 100;
+for (;;) {
+  if (i >= 110) break;
+  mainApp.innerHTML += `<p>${i}</p>`;
+  i++;
+} */
